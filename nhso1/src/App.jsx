2343ee06409a63b67abeb60699ca793a4378019f
tryntheworld <tryntheworld@gmail.com>
2024-07-16 16:18:57 +0700

    compo

--- nhso1/src/App.jsx
+++ nhso1/src/App.jsx
@@ -2,13 +2,13 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './App.css'
 import { faArrowAltCircleRight, faArrowDown, faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons'
-
+import Header from './sections/header'
 function App() {
   return (
    <div className='mt-14 mx-auto max-w-6xl grid grid-cols-[40%_60%]'>
     <div>
       <div className='sticky top-14 grid grid-rows[20%_40%_25%] h-[80vh]'>
-        
+        <Header/>
         <div className='flex flex-col gap-3 font-semibold'> 
           <div><FontAwesomeIcon className="mr-2 text-pink-500" icon={faArrowRight}/>ปสน.</div>
           <div>ตารางงาน</div>
