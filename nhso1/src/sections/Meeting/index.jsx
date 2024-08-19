2450ef7fa7ef58f2bb12c73510a70fe9b83a13b1
tryntheworld <tryntheworld@gmail.com>
2024-08-20 04:24:59 +0700

    tt

--- nhso1/src/sections/Meeting/index.jsx
+++ nhso1/src/sections/Meeting/index.jsx
@@ -1,7 +1,6 @@
 import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { formatThaiDate } from "../../utils";
-
 const Meeting =({api_meetList}) => {
     return (
                 <div>
